30e7ee5764f86c85c6c3e6bb9ec14f6535adfd27
Pablo <pablocarrara@yahoo.com.ar>
2023-11-15 18:44:47 -0300

    Comiteando los primeros scripts

---
+++ aws_s3_upload_delete_main.py
@@ -0,0 +1,32 @@
+#Main para subir un archivo o borrarlo de AWS S3
+#en aws_upload.py, aws_delete.py esta el codigo que debo importar
+
+import boto3
+import aws_s3_upload, aws_s3_delete
+
+
+
+
+s3 = boto3.resource('s3')
+file_name = 'joa_dia_tradicion.jpeg'
+bucket_name = 'mypyscriptsforjoa'
+
+
+print('''1- Subir archivo
+2- Borrar archivo
+3- Salir''')
+print()
+
+
+
+while True:
+    option = int(input('Opcion: '))
+
+    if option == 1:
+        aws_upload.upload_file(s3, bucket_name, file_name)
+    elif option == 2:
+        aws_delete.delete_file(s3, bucket_name, file_name)
+    elif option == 3:
+        break
+    else:
+        option = int(input('Opcion invalida. Opcion: '))
---
+++ aws_s3_upload_files_starts_with.py
@@ -0,0 +1,35 @@
+import os
+import boto3
+
+#Generator function that returns all files in the directory starting with a given string
+def list_to_upload(directory_path, starting_with_str):
+    for f in os.listdir(directory_path):
+        if os.path.isfile(f) and f.startswith(starting_with_str):
+            yield f
+
+
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+
+
+# directory containing the files to upload, bucket name where file will be uploaded, string variable used as condition
+directory_path = '/home/pablo/my_ubuntu_python_apps'
+bucket_name = 'mypyscriptsforjoa'
+starting_with_str = 'joa'
+
+#List all files in the directory starting with a given string
+#files_to_upload = [f for f in os.listdir(directory_path) if os.path.isfile(f) and f.startswith(starting_with_str)]
+
+#create the generator that returns all files in the directory starting with a given string
+files_to_upload = list_to_upload(directory_path, starting_with_str)
+
+#Upload each file to the S3 bucket
+for file_name in files_to_upload:
+    file_path = os.path.join(directory_path, file_name)
+    s3.upload_file(file_path, bucket_name, file_name)
+    print(f'Uploaded file {file_name} to {bucket_name}')
+
+
+
